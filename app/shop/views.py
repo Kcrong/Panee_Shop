f2e06a109a8df8a77e2e2f6d7d53535779647859
kcrong <kcrong@kim82536.pe.kr>
2016-04-13 18:37:14 +0900

    Recover 1 commit

--- app/shop/views.py
+++ app/shop/views.py
@@ -9,22 +9,22 @@ from sqlalchemy import desc, func
 from . import shop_blueprint
 from ..models import *
 
-product_sort = {
-    # 낮은 가격순
-    'low': Product.query.filter(Product.name.contains(query)).order_by('price').all(),
-    # 높은 가격순
-    'high': Product.query.filter(Product.name.contains(query)).order_by(desc('price')).all(),
-    # 인기도 순
-    'popular': [p for p, cnt in
-                db.session.query(Product, func.count(likes.c.person_id).label('like_total')).join(likes).group_by(
-                    Product).filter(Product.name.contains(query)).order_by(desc('like_total')).all()]
-}
-
 
 @shop_blueprint.route('/list/json')
 @shop_blueprint.route('/list/json/')
 @shop_blueprint.route('/list/json/<string:query>')
 def product_list_json(query=""):
+    product_sort = {
+        # 낮은 가격순
+        'low': Product.query.filter(Product.name.contains(query)).order_by('price').all(),
+        # 높은 가격순
+        'high': Product.query.filter(Product.name.contains(query)).order_by(desc('price')).all(),
+        # 인기도 순
+        'popular': [p for p, cnt in
+                    db.session.query(Product, func.count(likes.c.person_id).label('like_total')).join(likes).group_by(
+                        Product).filter(Product.name.contains(query)).order_by(desc('like_total')).all()]
+    }
+
     try:
         all_products = product_sort[request.args['sort']]
     except KeyError:
