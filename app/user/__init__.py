3b3993edd34034e8ee59d9377e32d33f4c04fa1d
kcrong <kcrong@kim82536.pe.kr>
2016-05-01 04:07:07 +0900

    Add restful source

--- app/user/__init__.py
+++ app/user/__init__.py
@@ -2,7 +2,10 @@
 # -*- coding:utf-8 -*-
 
 from flask import Blueprint
+from flask_restful import Api, reqparse
 
 user_blueprint = Blueprint('user', __name__)
+user_api = Api(user_blueprint)
+user_parser = reqparse.RequestParser()
 
 from . import views
--- app/user/views.py
+++ app/user/views.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from . import user_blueprint
+from flask_restful import Resource
+
+from . import user_api, user_parser
 
 
-@user_blueprint.route('/')
-def index():
-    return "User Index"
+@user_api.resource('/login')
+class Login(Resource):
+    def get(self):
+        return user_parser.parse_args()
+
+    def post(self):
+        user_parser.add_argument('userid', type=str, help='Only Int!')
+        return user_parser.parse_args()
