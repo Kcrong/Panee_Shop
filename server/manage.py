220225c4b73528352104c9fdf90adb83bd3e36ae
kcrong <kcrong@kim82536.pe.kr>
2016-04-30 18:14:40 +0900

    Successfully rendering sample.html

--- server/manage.py
+++ server/manage.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask.ext.script import Manager
 
-# from werkzeug.contrib.fixers import ProxyFix
-
-from app import create_app
-
+from app import create_app, manager
 
 app = create_app()
-# app.wsgi_app = ProxyFix(app.wsgi_app)
-manager = Manager(app)
+
 
 @manager.command
 def run():
     app.run()
+
 
 if __name__ == "__main__":
     manager.run()
