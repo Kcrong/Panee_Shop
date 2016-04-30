cf29275ec4be0229d38d8a46bc2af1d2fccd63a2
kcrong <kcrong@kim82536.pe.kr>
2016-04-30 16:43:59 +0900

    Remove list html rendering

--- app/shop/views.py
+++ app/shop/views.py
@@ -51,33 +51,6 @@ def product_list_json(query=""):
     return json.dumps(data)
 
 
-@shop_blueprint.route('/list')
-@shop_blueprint.route('/list/')
-@shop_blueprint.route('/list/<string:query>')
-def product_list(query=""):
-    product_sort = {
-        # 낮은 가격순
-        'low': Product.query.filter(Product.name.contains(query)).order_by('price').all(),
-        # 높은 가격순
-        'high': Product.query.filter(Product.name.contains(query)).order_by(desc('price')).all(),
-        # 인기도 순
-        'popular': [p for p, cnt in
-                    db.session.query(Product, func.count(likes.c.person_id).label('like_total')).join(likes).group_by(
-                        Product).filter(Product.name.contains(query)).order_by(desc('like_total')).all()]
-    }
-
-    try:
-        all_products = product_sort[request.args['sort']]
-    except KeyError:
-        all_products = product_sort['popular']
-
-    top_tag_list = Tag.query.filter_by(top=True).all()
-
-    return render_template('shop/main.html',
-                           all_products=all_products,
-                           top_tag=top_tag_list)
-
-
 @shop_blueprint.route('/image/<path:filename>')
 def shop_image(filename):
     return send_from_directory(shop_blueprint.root_path + '/image/', filename)
