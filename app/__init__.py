c46bfe7c5bfda162be74c0b8e7e7165d1b2bd991
kcrong <kcrong@kim82536.pe.kr>
2016-05-01 20:27:52 +0900

    Register shop, api_explorer blueprint at app

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,21 @@
 from flask import Flask
 from flask.ext.script import Manager
 
+from .static_string import *
+
 app = Flask(__name__)
 
 
 def create_app():
     from .main import main_blueprint
     from .user import user_blueprint
+    from .shop import shop_blueprint
+    from .api_explorer import explorer_blueprint
 
     app.register_blueprint(main_blueprint)
-    app.register_blueprint(user_blueprint, url_prefix='/user')
+    app.register_blueprint(user_blueprint, url_prefix=USER_URL_PREFIX)
+    app.register_blueprint(shop_blueprint, url_prefix=SHOP_URL_PREFIX)
+    app.register_blueprint(explorer_blueprint, url_prefix=EXPLORER_URL_PREFIX)
 
     app.config.from_pyfile('../config.py')
 
