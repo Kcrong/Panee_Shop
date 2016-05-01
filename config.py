fc131af13f838fcd6e03744491cbe51a82b04625
kcrong <kcrong@kim82536.pe.kr>
2016-05-01 21:38:07 +0900

    Add secret key

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os.path
 DEBUG = True
 STATIC_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app', 'client')
 USE_METHOD = ['GET', 'POST', 'PUT', 'DELETE']
+SECRET_KEY = 'development-key'
