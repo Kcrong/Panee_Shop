6a4dce764a71226e54945ef4029d63f356a29e3a
kcrong <kcrong@kim82536.pe.kr>
2016-05-01 22:17:22 +0900

    Improve code with login_manager

--- app/user/views.py
+++ app/user/views.py
@@ -7,30 +7,33 @@ from flask_restful import Resource, reqparse
 from app.static_string import USER_LOGIN_URL, USER_LOGOUT_URL
 from config import USE_METHOD
 from . import user_api
+from app.models import User
+from app.static_string import json_message
+from .login_manager import login_required, login_user, logout_user
 
 
 @user_api.resource(USER_LOGIN_URL)
 class Login(Resource):
     def __init__(self):
-        self.parser = user_parser[request.method]
+        self.parser = user_parser[USER_LOGIN_URL][request.method]
 
     def get(self):
-        """
-        Logout User
-        """
-        return user_parser.parse_args()
+        login_user('asdf')
+        return json_message()
 
+    @login_required
     def post(self):
-        return self.parser.args[0].name
+        args = self.parser.parse_args()
+        u = User.query.filter_by(userid=args['userid'], userpw=args['userpw'], active=True).first_or_404()
 
 
 @user_api.resource(USER_LOGOUT_URL)
 class Logout(Resource):
-    def __init__(self):
-        self.parser = user_parser[request.method]
 
+    @login_required
     def get(self):
-        return "User Logout"
+        logout_user()
+        return json_message()
 
 
 user_parser = {
@@ -40,4 +43,10 @@ user_parser = {
 
 # USER_LOGIN - POST
 parser = user_parser[USER_LOGIN_URL]['POST']
-parser.add_argument('userid', type=str, help='Need Userid')
+parser.add_argument('userid', type=str, help='Need String Userid', required=True)
+parser.add_argument('userpw', type=str, help='Need String Userpw', required=True)
+
+# USER_LOGOUT - POST
+parser = user_parser[USER_LOGOUT_URL]['POST']
+parser.add_argument('userid', type=str, help='Need String Userid', required=True)
+parser.add_argument('userpw', type=str, help='Need String Userpw', required=True)
