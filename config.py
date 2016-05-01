d32bbe618103347246ed2d47a6c5df0940bcfbfc
kcrong <kcrong@kim82536.pe.kr>
2016-05-01 22:13:58 +0900

    Separate debug env and deploy env

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
-import os.path
+import os
 
-DEBUG = True
+DEBUG = os.environ['FLASK_DEBUG'] == 'true'
+if DEBUG:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/testdb.db'
+else:
+    SQLALCHEMY_DATABASE_URI = 'mysql://panee:panee123@localhost:3306/panee?charset=utf8'
 STATIC_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app', 'client')
 USE_METHOD = ['GET', 'POST', 'PUT', 'DELETE']
 SECRET_KEY = 'development-key'
